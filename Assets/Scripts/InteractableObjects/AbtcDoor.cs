9d0f081c0373ba25c75dab4fb4e750d5cc6fe448
SantiagaGaming <ganychev92@gmail.com>
2022-10-03 17:18:59 +0300

    Temp

--- Assets/Scripts/InteractableObjects/AbtcDoor.cs
+++ Assets/Scripts/InteractableObjects/AbtcDoor.cs
@@ -48,7 +48,7 @@ public class AbtcDoor : BaseObject
             while (y >= -107)
             {
                 transform.localRotation = Quaternion.Euler(-90, y, 17.137f);
-                y--;
+                y--g;
                 yield return new WaitForSeconds(0.01f);
             }
 
