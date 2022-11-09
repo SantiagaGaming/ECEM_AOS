5e2b0068e8488fb8bc3dc95eec9ce9d1d5369bcf
Ilya <ganychev92@gmail.com>
2022-11-09 16:33:36 +0300

    Bug fiex

--- Assets/Scripts/InteractableObjects/Buttons/BigWatchButton.cs
+++ Assets/Scripts/InteractableObjects/Buttons/BigWatchButton.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BigWatchButton : BaseButton
-{w
+{
     [SerializeField] private PlayerToMenuAndArmTeleportController _menuArmController;
 
     public override void OnClicked(InteractHand interactHand)
